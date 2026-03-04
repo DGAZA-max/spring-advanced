9b052bb2bde437ccf9b35673096bbfbd4eb23b87
DGAZA-max <name@naver.com>
2026-03-04 14:11:13 +0900

    레벨 1: ArgumentResolver feat: WebConfig 구현 fix: application.yml 내 잘못된 Base64 인코딩 오류 수정(한글 포함 데이터 인코딩 오류로 인한 비밀번호 수정), 나머지는 주석만 추가

--- src/main/java/org/example/expert/config/AuthUserArgumentResolver.java
+++ src/main/java/org/example/expert/config/AuthUserArgumentResolver.java
@@ -7,13 +7,19 @@ import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.user.enums.UserRole;
 import org.springframework.core.MethodParameter;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+
+@Component
+// 컨트롤러 메서드의 파라미터를 특정 객체(인증된 유저 정보)로 자동 변환해서 주입해 주는 역할
 public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
 
+
+    // 대상 확인 : 내가 처리할 수 있는 타입인지 확인
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         boolean hasAuthAnnotation = parameter.getParameterAnnotation(Auth.class) != null;
@@ -27,6 +33,9 @@ public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
         return hasAuthAnnotation;
     }
 
+
+    // 조건이 맞다면 JwtFilter 같은 곳에서 HttpServletRequest에 저장해 둔 유저 정보를 꺼내
+    // 실제 객체로 만들어 컨트롤러에 전달
     @Override
     public Object resolveArgument(
             @Nullable MethodParameter parameter,
