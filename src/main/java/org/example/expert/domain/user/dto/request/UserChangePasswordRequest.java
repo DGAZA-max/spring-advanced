d5e65a6e4d9fbd1d9e935d4f786fc4ff0c190478
DGAZA-max <name@naver.com>
2026-03-05 19:29:04 +0900

    레벨 3-1: 코드 개선 퀴즈 - Validation fix: UserService.java -> "changePassword"-"if throw" delete UserChangePasswordRequest.java -> ""@Pattern, @Size" add UserController.java-changePassword -> "@Valid" add

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,9 +1,15 @@
 package org.example.expert.domain.user.dto.request;
 
+import jakarta.persistence.Column;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Value;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 
 @Getter
 @NoArgsConstructor
@@ -13,5 +19,9 @@ public class UserChangePasswordRequest {
     @NotBlank
     private String oldPassword;
     @NotBlank
+    @Pattern(regexp = ".*\\d.*")
+    @Pattern(regexp = ".*[A-Z].*")
+    @Size(min=8)
     private String newPassword;
+
 }
